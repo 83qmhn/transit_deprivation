84486f4f0ddfd06daad140c3454e2b6c7b8161f1
Lois <loislichuan@gmail.com>
2021-02-04 22:58:57 +1300

    improved server cod

---
+++ app/backend/README.md
@@ -0,0 +1,17 @@
+App Backend
+
+Nodejs server to provide transit data to app frontend
+
+Setup
+
+ln -s ../../data/backend data
+
+Override environment settings if required via: cp .env .env.local
+
+Install
+
+yarn install
+
+Run
+
+bash run.sh
--- app/backend/server.js
+++ app/backend/server.js
@@ -1,21 +1,32 @@
+const dotenvLoad = require('dotenv-load');
+dotenvLoad();
+
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
+const port = process.env.PORT;
+const appHost = process.env.APP_HOST;
+const appPort = process.env.APP_PORT;
+
+const server = http.createServer(function (req, res) {
     let u = url.parse(req.url, true);
     if (u.pathname === "/transit"){
         let q = u.query;
-        let filename = `./data/${q.region}/${q.location}.json`;
+        let filename = `./data/${q.direction}/${q.region}/${q.location}.json`;
         fs.readFile(filename, function(err, data) {
             if (err) {
-                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.writeHead(404, {
+                    "Content-Type": "text/html",
+                    "Access-Control-Allow-Origin": `http://${appHost}:${appPort}`,
+                    "Access-Control-Allow-Methods": "GET",
+                    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+                });
                 return res.end();
             }
-            console.log("serving" + req.headers.host);
             res.writeHead(200, {
-                'Content-Type': 'application/json',
-                "Access-Control-Allow-Origin": "*",
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": `http://${appHost}:${appPort}`,
                 "Access-Control-Allow-Methods": "GET",
                 "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
             });
@@ -23,7 +34,17 @@ http.createServer(function (req, res) {
             return res.end();
         });
     } else {
-        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.writeHead(404, {
+            "Content-Type": "text/html",
+            "Access-Control-Allow-Origin": `http://${appHost}:${appPort}`,
+            "Access-Control-Allow-Methods": "GET",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        });
         res.end();
     }
-}).listen(8081);
+});
+
+server.listen(port, () => {
+    const address = server.address();
+    console.log(`Server listening on port ${address.port} for requests from ${appHost}:${appPort}`);
+});
